e8d9f7266c8a5b2d6655d85b1ae018034e88ab72
kalsi <sachinkalsi15@gmail.com>
2018-12-23 17:42:41 +0530

    as

--- run.py
+++ run.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from app import app
 
-app = Flask(__name__)
-
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return "<h3>Sachin Kalsig</h3>"
+    return "<h3>Sachin Kalsi awesome</h3>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+#
+#
+# from flask import Flask
+#
+# app = Flask(__name__)
+#
+# @app.route('/')
+# def index():
+#     return "<h3>Sachin Kalsig</h3>"
